0c223605ad48960ab6cd25823b909e207afa68b3
Patrick Marsceill <marsceill@happycog.com>
2014-09-11 19:12:36 -0400

    removing livereload from JS

--- a/j/plugins/livereload.js
+++ a/j/plugins/livereload.js
@@ -1,2 +1,2 @@
 //livereload
-document.write('<script src="http://' + window.location.hostname + ':35729/livereload.js?snipver=1" type="text/javascript"> <\/script>');
+// document.write('<script src="http://' + window.location.hostname + ':35729/livereload.js?snipver=1" type="text/javascript"> <\/script>');
